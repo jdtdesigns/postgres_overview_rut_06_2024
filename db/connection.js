f98bd6182aa4b422e2f1ec69c7531b51235e117f
JD Tadlock <jt7903@gmail.com>
2024-07-26 15:05:08 -0400

    added render db connection

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,6 @@
 const { Pool } = require('pg');
 const is_prod = process.env.NODE_ENV === 'production';
 
-console.log('IS_PROD', is_prod);
-console.log('DB_URL', process.env.DB_URL);
-
 const localConnection = {
   user: 'postgres',
   password: 'pass',
@@ -11,12 +8,13 @@ const localConnection = {
 };
 
 const renderConnection = {
-  host: 'dpg-cqhuq29u0jms739dn570-a',
-  user: 'jd',
-  password: 'B5ppbPBxTfyYPxbI7JcauN1xmY8cSo4p',
-  database: 'testing_db_3h13'
+  connectionString: process.env.DB_URL
 }
 
-const client = new Pool(is_prod ? localConnection : renderConnection);
+const connectObj = is_prod ? localConnection : renderConnection;
+
+console.log('connect object', connectObj);
+
+const client = new Pool(connectObj);
 
 module.exports = client;
