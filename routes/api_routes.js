870dc690ed7a96ed5eafacf33faeb79b3813097f
JD Tadlock <jt7903@gmail.com>
2024-07-29 13:12:03 -0400

    finished basic student app api

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const Query = require('../lib/Query');
 
+// GET ROUTES
+
 // GET route to send all students
 // localhost:3333/api/students
 router.get('/students', async (request, response) => {
@@ -8,6 +10,17 @@ router.get('/students', async (request, response) => {
 
   response.json(students);
 });
+
+// GET route to send all courses
+// localhost:3333/api/courses
+router.get('/courses', async (request, response) => {
+  const courses = await Query.getCourses();
+
+  response.json(courses);
+});
+
+
+// CREATE ROUTES
 
 // POST Create a course
 // post request to localhost:3333/api/courses
@@ -20,5 +33,18 @@ router.post('/courses', async (request, response) => {
     message: 'Course created successfully!'
   });
 });
+
+// POST Create a student
+// post request to localhost:3333/api/students
+router.post('/students', async (request, response) => {
+  const formData = request.body;
+
+  await Query.addStudent(formData);
+
+  response.json({
+    message: 'Student created successfully!'
+  });
+});
+
 
 module.exports = router;
