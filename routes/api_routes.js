b9228deb7ae40fa91416e829beb9a0b3f009ce30
JD Tadlock <jt7903@gmail.com>
2024-07-29 12:32:50 -0400

    finished post course route

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,32 +1,24 @@
 const router = require('express').Router();
-const client = require('../db/connection');
+const Query = require('../lib/Query');
 
 // GET route to send all students
 // localhost:3333/api/students
 router.get('/students', async (request, response) => {
-  const sql = `
-    SELECT
-      s.id AS student_id,
-      CONCAT(s.first_name, ' ', s.last_name) AS full_name,
-      c.id AS course_id,
-      course_name,
-      course_type,
-      g.id AS group_id,
-      group_name,
-      gleaders.first_name AS group_leader
-      FROM students AS s
-        JOIN courses AS c
-          ON s.course_id = c.id
-        LEFT JOIN groups AS g
-          ON s.group_id = g.id
-        LEFT JOIN students AS gleaders
-          ON s.group_leader_id = gleaders.id
-      ORDER BY student_id
-    `;
+  const students = await Query.getStudents();
 
-  const data = await client.query(sql);
+  response.json(students);
+});
 
-  response.json(data.rows);
+// POST Create a course
+// post request to localhost:3333/api/courses
+router.post('/courses', async (request, response) => {
+  const formData = request.body;
+
+  await Query.addCourse(formData);
+
+  response.json({
+    message: 'Course created successfully!'
+  });
 });
 
 module.exports = router;
